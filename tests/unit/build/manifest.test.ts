e42446a95064c7996ac601746061db3233db79b9
kuschzzp <superkusch@foxmail.com>
2026-06-26 15:35:14 +0800

    feat: add extension icons

--- tests/unit/build/manifest.test.ts
+++ tests/unit/build/manifest.test.ts
@@ -8,4 +8,14 @@ describe("MV3 manifest", () => {
     expect(manifest.background.service_worker).toBe("background.js");
     expect(manifest.content_scripts[0].js).toEqual(["content.js"]);
   });
+
+  it("declares extension and toolbar icons", () => {
+    expect(manifest.icons).toEqual({
+      "16": "icons/icon-16.png",
+      "32": "icons/icon-32.png",
+      "48": "icons/icon-48.png",
+      "128": "icons/icon-128.png"
+    });
+    expect(manifest.action.default_icon).toEqual(manifest.icons);
+  });
 });
